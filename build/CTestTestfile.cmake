eee3cae370c6164721741ed210966bc5c5361236
Eduardo <eduardo.camara@icomp.ufam.edu.br>
2023-09-17 14:35:18 -0400

    Git Ignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/eduardo/Dropbox/Projects/cppProjects/project_1
-# Build directory: /Users/eduardo/Dropbox/Projects/cppProjects/project_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
